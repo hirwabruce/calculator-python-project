d96addfbd579c7af665f0db143bddd1ecf9f958a
Bruce Hirwa <hirwabruce00@gmail.com>
2026-06-16 08:43:18 +0200

    Added a while loop

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from operations import add, subtract, multiply, divide
 print("Calculator Operations:")
-operations = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-x = float(input("Enter the first number: "))
-y = float(input("Enter the second number: "))
-if operations == "add":
-    result = add(x, y)
-elif operations == "subtract":
-    result = subtract(x, y)
-elif operations == "multiply":
-    result = multiply(x, y)
-elif operations == "divide":
-    result = divide(x, y)
-print(f"The result of {operations} is: {result}")
+while True:
+    operations = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+    if operations == "exit":
+        break
+    x = float(input("Enter the first number: "))
+    y = float(input("Enter the second number: "))
+    if operations == "add":
+        result = add(x, y)
+    elif operations == "subtract":
+        result = subtract(x, y)
+    elif operations == "multiply":
+        result = multiply(x, y)
+    elif operations == "divide":
+        result = divide(x, y)
+    else:        
+        print("Invalid operation. Please try again.")
+        continue
+    print(f"The result of {operations} is: {result}")
